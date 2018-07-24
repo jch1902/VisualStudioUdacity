aa5371d483c6d32f3e997795a4581a84e1c10ffa
jch1902 <jasper.d39c@gmail.com>
2018-07-24 15:20:34 -0700

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "banana";
+var vessel = "cone";
+var toppings = "sprinkles";
+
+if (flavor == ("chocolate" || "vanilla") && (vessel == "cone" || "bowl") && (toppings == 
+"sprinkles" || "peanuts")){
+    console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings + ".");
+}
+
